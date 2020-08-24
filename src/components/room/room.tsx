576703a955dab7e23310ebf63a52aec3cb05b6f5
Tuzilow <1176281967@qq.com>
2020-08-24 21:26:22 +0800

    初始化项目

---
+++ src/components/room/room.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Room() {
+  return <div>房间页</div>;
+}
---
+++ src/components/user/user.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function User() {
+  return <div>用户页</div>;
+}
